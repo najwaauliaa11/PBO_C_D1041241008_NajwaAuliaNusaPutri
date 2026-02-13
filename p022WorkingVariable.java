1e6558f28b733a6dae3dcf94b8536c1b28aa6e57
najwaaulila11 <najwaaulia1102@gmail.com>
2026-02-13 21:52:55 +0700

    First commit

---
+++ p022WorkingVariable.java
@@ -0,0 +1,21 @@
+/*
+NIM : D1041241008
+Nama : Najwa Aulia Nusa Putri
+Program untuk variabel
+ */
+
+public class p022WorkingVariable {
+    public static void main (String []args){
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        // Menampilkan nilai awaL variabel
+        System.out.println ("Nilai awal angka: "+ angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: "+ angka);
+    }
+}
