3ec43a9385827ad749af5ed1ff12df0315db9fc8
najwaaulila11 <najwaaulia1102@gmail.com>
2026-02-26 23:54:37 +0700

    Menambahkan latiha 3 pada pertemuan 8

---
+++ PERTEMUAN 8/LatihanTotalGenap.java
@@ -0,0 +1,23 @@
+/*
+NIM : D1041241008
+Nama : Najwa Aulia Nusa Putri
+Program untuk latihan Continue - Hitung Total Bilangan Genap
+*/
+public class LatihanTotalGenap {
+    public static void main(String[] args) {
+        System.out.println("=== TOTAL BILANGAN GENAP 1-20 ===");
+            int total =0;
+        for (int i=1; i<=20; i++){
+            if (i%2!=0){
+                continue;   
+            }
+             total +=i;
+            System.out.println("Tambah: "+ i + " -> Total: "+ total );
+            
+        }
+        System.out.println("---");
+        System.out.println("Total akhir: " + total);
+        System.out.println();
+    }
+    
+}
